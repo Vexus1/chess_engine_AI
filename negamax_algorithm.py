3b603128567e2f570347c15ab614f9bf2cae1242
Vexus1 <adriangalik123@gmail.com>
2024-08-05 14:44:58 +0200

    eliminate the zugzwang problem in null move heuristic

--- negamax_algorithm.py
+++ negamax_algorithm.py
@@ -36,7 +36,7 @@ class ChessAi:
            to simplify the implementation of the minimax algorithm'''
         if depth == 0 or self.board.is_game_over():
             return color * self.evaluate_board
-        self.null_move_pruning(depth, alpha, beta, color)
+        self.null_move_heuristic(depth, alpha, beta, color)
         max_eval = float('-inf')
         for move in self.board.legal_moves:
             self.board.push(move)
@@ -52,14 +52,19 @@ class ChessAi:
                           beta: int, color: int) -> int:
         '''Implements null move heuristic to enhance the alpha-beta pruning
            in the Negamax algorithm. This technique involves making
-           a 'null' move (no actual move) and searching at a reduced depth.'''
-        if depth >= (self.null_move_reduction + 1) and not self.board.is_check():
-            self.board.push(chess.Move.null())
-            null_move_eval = -self.negamax(depth - 1 - self.null_move_reduction,
-                                           -beta, -alpha, -color)
-            self.board.pop()
-            if null_move_eval >= beta:
-                return beta
+           a 'null' move (no actual move) and searching at a reduced depth.
+           Additionally eliminates situations prone to zugzwang'''
+        if (depth < (self.null_move_reduction + 1) or
+            self.board.is_check() or
+            self.board.legal_moves.count() == 1 or
+            len(self.board.piece_map()) <= 4):
+            return
+        self.board.push(chess.Move.null())
+        null_move_eval = -self.negamax(depth - 1 - self.null_move_reduction,
+                                        -beta, -alpha, -color)
+        self.board.pop()
+        if null_move_eval >= beta:
+            return beta
             
     @time_it
     def decision_function(self, depth: int) -> Move:
@@ -85,7 +90,7 @@ class ChessAi:
     @property
     def decision_tree_depth(self) -> int:
         '''Determines the depth of the decision tree for the Min-Max algorithm.'''
-        return 5
+        return 6
     
     @property
     def player(self) -> str:
